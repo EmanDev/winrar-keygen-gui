3ddcfef6f503f2a72b5dfb85605c573887f126ad
Eman <game.and.windows.tutorial1@gmail.com>
2021-06-13 13:27:32 +0800

    Initial Release
    - Added files

---
+++ Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 97)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(284, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generate License"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "License name:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 26)
+        Me.TextBox1.MaxLength = 64
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(284, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Choose license type:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Single PC Usage License", "Multiple PC Usage License"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 70)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(284, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 141)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WinRAR Keygen"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Public Class MyUtilities
+        Shared Sub RunCommandCom(ByVal command As String, ByVal arguments As String, ByVal permanent As Boolean)
+            Dim p As Process = New Process()
+            Dim pi As ProcessStartInfo = New ProcessStartInfo()
+            pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + arguments
+            pi.FileName = "cmd.exe"
+            p.StartInfo = pi
+            p.Start()
+        End Sub
+    End Class
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox("Be sure to save the license file as 'rarreg.key' in order to register the license.", MsgBoxStyle.Information, "WinRAR Keygen")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SaveFileDialog1.Filter = "KEY File (*.key)|*.key"
+        SaveFileDialog1.FileName = "rarreg.key"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim xpath As String = Path.Combine(Path.GetTempPath(), "winrar-keygen.exe")
+            File.WriteAllBytes(xpath, My.Resources.winrar_keygen)
+            Dim username As String = TextBox1.Text
+            Dim lictype As String = ComboBox1.Text
+            Dim q As String = Chr(34)
+            Dim p As String = Chr(62)
+            Dim sb As String = " "
+            Dim gen As String = q + username + q + sb + q + lictype + q + sb + p + sb + SaveFileDialog1.FileName
+
+            MyUtilities.RunCommandCom(xpath, gen, False)
+        Else
+            MsgBox("Cancelled! User has cancelled the operation.", MsgBoxStyle.Information, "WinRAR Keygen")
+        End If
+    End Sub
+End Class
